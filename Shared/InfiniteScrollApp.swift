2ba0d674e414c2392a6514e3dc3cf7cf4e1024e5
Thiago lovatine <thiagolovatine@gmail.com>
2021-09-26 15:11:51 -0300

    Initial Commit

---
+++ Shared/InfiniteScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteScrollApp.swift
+//  Shared
+//
+//  Created by Thiago on 26/09/21.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
